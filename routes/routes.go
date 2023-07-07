4791b877186732d05dad57cf8739c9e8b78952b4
Melih Emre Güler <melihguler@Melihs-MacBook-Pro.local>
2023-07-07 17:29:43 +0300

    add Put and Delete Methods

--- routes/routes.go
+++ routes/routes.go
@@ -10,4 +10,6 @@ func RegisterHandlers(r *mux.Router) {
 
 	r.HandleFunc("/city", handlers.GetCity).Methods(http.MethodGet)
 	r.HandleFunc("/city", handlers.PostCity).Methods(http.MethodPost)
+	r.HandleFunc("/city", handlers.PutCity).Methods(http.MethodPut)
+	r.HandleFunc("/city", handlers.DeleteCity).Methods(http.MethodDelete)
 }
